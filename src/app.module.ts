792fb1377ff8bd2e35e420e66c88c25cc25db21c
MyroslavSB <Myroslavsobchyshyn@gmail.com>
2024-06-03 18:02:39 +0200

    changes made

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,3 +43,18 @@ import {join} from 'path'
 })
 export class AppModule {
 }
+
+// TypeOrmModule.forRootAsync({
+//     imports: [ConfigModule],
+//     useFactory: (configService: ConfigService) => ({
+//         type: 'mysql',
+//         host: configService.get('DB_HOST'),
+//         port: configService.get('DB_PORT'),
+//         username: configService.get('DB_USERNAME'),
+//         password: configService.get('DB_PASSWORD'),
+//         database: configService.get('DB_NAME'),
+//         entities: entities,
+//         synchronize: true,
+//     }),
+//     inject: [ConfigService]
+// }),
